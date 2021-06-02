63455fb6af6de8c0108562a35b0e4dcff743084d
Fabiana Oliveira <fabi.ana_deoliveira@outlook.com>
2021-06-02 15:16:38 -0300

    Lição de casa 02/06/21

---
+++ Atividascasa/Spring 02-06-21/famarciamagica/famarciamagica/src/test/java/com/Farmacia/famarciamagica/FamarciamagicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Farmacia.famarciamagica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamarciamagicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
